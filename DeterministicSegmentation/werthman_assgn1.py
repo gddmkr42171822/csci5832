2cec6bb94371451aee3814699de934e919ba4f60
Bob <Bob@engr2-19-17-dhcp.int.colorado.edu>
2015-11-12 14:56:18 -0700

    Renamed directory

---
+++ DeterministicSegmentation/werthman_assgn1.py
@@ -0,0 +1,396 @@
+'''
+Created on Sep 9, 2015
+CSCI 5832: Assignment 1
+@author: Robert Werthman
+
+Pseudocode
+------------
+
+Part 1
+-------
+
+Script should take in two arguments: the path to the word list and the path for the hashtag list
+(the absolute path needs to be found, not dependent on the os)
+
+Step 1: Read in list of words (read in a maximum of 75000 words) and hashtags
+read in a file line by line and find the first alphanumeric word on each line
+store each word in a dictionary
+
+Recursive step?
+Step 3: break up hashtag in mulitple words using MaxMatch algorithm
+find largest word in hashtag from list of words
+append it to a string or write it a file without a newline 
+repeat until at the end the hashtag
+output maxmatched hashtags to a file
+
+Part 2
+---------
+
+Create a working Word Error Rate that takes two files as input: 
+    the hashtags outputted by the maxmatch and what the hashtag should really look like based on the english language
+    
+Change min edit algo to make it work
+Compute WER from result of minimum edit distance divided by length of gold standard list
+
+Part 3
+-------
+change maxmatch to get a better word error rate
+possible changes:
+
+Changes to MaxMatch strategy
+    1) instead of taking the largest word that fits take the smallest word greater >= 2 characters *this was not an improvement*
+    2) maxmatch backwards instead of forwards * improved WER from .60 to .35*
+    3) don't add spaces after characters not found in corpus *improved WER from .39 to .35*
+    4) run maxmatch both ways and take output with fewer words, if same number take reverse maxmatch
+    5) check for the largest string in any part of the hashtag instead of just at the front or end *improved WER from .35 to .29*
+    
+Changes to gready nature
+    1) if there is a single letter at the end of the string combine it with the previous word *greedy nature changed, improved WER from .66 to .60* 
+
+Changes to lexicon
+    1) do not take single characters to be words in the word list/lexicon *this did not improve anything*
+    2) clean up lexicon/corpus/wordlist to not include typos
+    3) remove and insert as many words as possible to correct the output * improve WER from .29 to .11 *
+
+    
+    
+
+
+'''
+
+import re
+import sys
+import os
+
+
+
+def readWordsFromFile(filePath, limitNumWords, numWordsLimit):
+    '''
+    Function that opens and reads a file line by line and appends the first word
+    it sees on each line to a list
+    
+    @type: string
+    @param filePath: the path to the file to open on the filesystem
+    @type: int
+    @param limitNumWords: the maximum number of lines it should read from the file
+    @type: boolean
+    @param numWordLimit: a boolean as to whether the number of lines should be limited or not
+    
+    @rtype: list
+    @return: a list of the first words from each line in the file
+    '''
+    wordlist = []
+    #create a regular expression to match the first word in a line of the file
+    pattern = re.compile('\w+')
+    #read the file into memory
+    with open(filePath, 'r') as f:
+        #go through the file line by line and get the first word out of each line
+        #if we aren't limited by the number of words extract a word out of every line
+        if not limitNumWords:
+            for line in f:
+                match = re.search(pattern, line)
+                if match:
+                    wordlist.append(match.group(0))
+        else:
+            counter = 0
+            for line in f:
+                #make sure the number of words in the set is under the number of word limit keep
+                #adding them to the set
+                if counter < numWordsLimit:
+                    match = re.search(pattern, line)
+                    if match:
+                        wordlist.append(match.group(0))
+                else:
+                    break
+                counter += 1
+    return wordlist
+
+def maxMatch(hashtag, wordlist, maxmatchedHashtag):
+    '''
+    Function performs the max match algorithm on a hashtag.
+    
+    @type string:
+    @param hashtag: a hashtag without the #
+    @type list:
+    @param wordlist: the lexicon/corpus used to find words in the hashtag
+    @type string:
+    @param maxmatchedHashtag: the part of the hashtag that max match has already gone through
+    
+    @rtype: string
+    @return: the result of perform max match on the entire hashtag     
+    '''
+    substringList = []
+    largestWord = ""
+    for word in wordlist:
+        #make a list of the words that start at the beginning of the hashtag
+        if hashtag.startswith(word):
+            substringList.append(word)
+    #if words were found in the wordlist that fit into the hashtag 
+    if len(substringList) != 0:
+        #find largest string in the substring list
+        largestWord = max(substringList, key=len)
+        #if there are still characters after the largest word is found 
+        #look through the word list again for the next largest word
+        if len(largestWord) < len(hashtag):
+            maxmatchedHashtag = maxmatchedHashtag + largestWord + " "
+            #return is necessary otherwise maxmatchedHashtag is discarded
+            return maxMatch(hashtag[len(largestWord):], wordlist, maxmatchedHashtag)
+        #if the largest word goes to the end the hashtag add it to the maxmatchHashtag
+        elif len(largestWord) == len(hashtag):
+            return (maxmatchedHashtag + largestWord)
+        else:
+            #this is for the largest word being longer than the hashtag
+            #which should never happen
+            print("Error: this maxMatch function statement should never be reached")
+    else:
+        #if no more words in the word list fit into the hashtag move over a character and try again
+        #if the remaining hashtag is greater than a single character send it back to maxmatch
+        if len(hashtag) > 1:
+            maxmatchedHashtag = maxmatchedHashtag + hashtag[0] + " "
+            return maxMatch(hashtag[1:], wordlist, maxmatchedHashtag)
+        else:
+            #otherwise just add the single character as a new word to the maxmatchedHashtag
+            return (maxmatchedHashtag + hashtag)
+
+def backwardsMaxMatch(hashtag, wordlist, maxmatchedHashtag):
+    '''
+    Function performs the max match algorithm on a hashtag but instead
+    of starting at the beginning it starts at the end.
+    
+    @type string:
+    @param hashtag: a hashtag without the #
+    @type list:
+    @param wordlist: the lexicon/corpus used to find words in the hashtag
+    @type string:
+    @param maxmatchedHashtag: the part of the hashtag that max match has already gone through
+    
+    @rtype: string
+    @return: the result of perform max match on the entire hashtag     
+    '''
+    substringList = []
+    largestWord = ""
+    for word in wordlist:
+        #make a list of the words that start at the end hashtag
+        #IMPROVEMENT: RUN MAXMATCH BACK TO FRONT
+        if hashtag.endswith(word):
+            substringList.append(word)
+    #if words were found in the wordlist that fit into the hashtag
+    if len(substringList) != 0:
+        #find largest string in the substring list
+        largestWord = max(substringList, key=len)
+        #if there are still characters after the largest word is found 
+        #look through the word list again for the next largest word
+        if len(largestWord) < len(hashtag):
+            #make sure single characters aren't alone so don't put a space between them
+            if len(largestWord) > 1:
+                maxmatchedHashtag = " " + largestWord + maxmatchedHashtag
+            else:
+                maxmatchedHashtag = largestWord + maxmatchedHashtag
+            return backwardsMaxMatch(hashtag[:-len(largestWord)], wordlist, maxmatchedHashtag)
+        #if the largest word takes up the entire hashtag
+        elif len(largestWord) == len(hashtag):
+            #if the largestWord is greater than a single character create a new word
+            if len(largestWord) > 1:
+                #return (maxmatchedHashtag + largestWord)
+                return (largestWord + maxmatchedHashtag)
+            else:
+                #otherwise add the single character to the previous word
+                #IMPROVEMENT: DON'T LET SINGLE CHARACTERS START THE WORD
+                return (largestWord + maxmatchedHashtag.lstrip())
+        else:
+            #this is for the largest word being longer than the hashtag
+            #which should never happen
+            print("Error: this maxMatch function statement should never be reached")
+    else:
+        #if no more words in the word list fit into the hashtag move over a character and try again
+        #if the remaining hashtag is greater than a single character send it back to maxmatch
+        if len(hashtag) > 1:
+            #IMPROVEMENT: don't add spaces after characters not found in wordlist
+            maxmatchedHashtag = hashtag[-1] + maxmatchedHashtag
+            return backwardsMaxMatch(hashtag[:-1], wordlist, maxmatchedHashtag)
+        else:
+            #if the remaining hashtags is a single character just append it to the previous word
+            #IMPROVMENT: DON'T LET SINGLE CHARACTErS BE BY THEMSELVES AT THE BEGINNING OF A WORD
+            return (hashtag + maxmatchedHashtag.lstrip())    
+        
+def anyMaxMatch(hashtag, wordlist, maxmatchedHashtag):
+    '''
+    Function performs the max match algorithm on a hashtag but instead of starting
+    at the beginning it finds the largest word anywhere in the hashtag.
+    
+    @type string:
+    @param hashtag: a hashtag without the #
+    @type list:
+    @param wordlist: the lexicon/corpus used to find words in the hashtag
+    @type string:
+    @param maxmatchedHashtag: the part of the hashtag that max match has already gone through
+    
+    @rtype: string
+    @return: the result of perform max match on the entire hashtag     
+    '''
+    #if the hashtag is empty then don't do anything
+    if not hashtag:
+        return maxmatchedHashtag
+    
+    substringList = []
+    largestWord = ""
+    for word in wordlist:
+        #make a list of the words that start anywhere in the hashtag
+        if word in hashtag:
+            substringList.append(word)
+    #if words were found in the wordlist that fit into the hashtag
+    if len(substringList) > 0:
+        #find largest string in the substring list
+        largestWord = max(substringList, key=len)
+        #split the string in half based on the largest word
+        splitSides = hashtag.split(largestWord, 1)
+        #take each half and perform max match on it
+        if len(splitSides[0]) < 2:
+            #just append a single character and not put a space between it and another word
+            maxmatchedHashtag = splitSides[0] + largestWord
+        else:
+            maxmatchedHashtag = anyMaxMatch(splitSides[0], wordlist, maxmatchedHashtag) + " " + largestWord
+        if len(splitSides[1]) < 2:
+            #just append a single character and not put a space between it and another word
+            maxmatchedHashtag = maxmatchedHashtag + splitSides[1]
+        else:
+            maxmatchedHashtag = maxmatchedHashtag + " " + anyMaxMatch(splitSides[1], wordlist, maxmatchedHashtag)
+        return maxmatchedHashtag
+    else:
+        #otherwise just return what is left 
+        return hashtag
+        
+def checkCommandLineArgs(): 
+    '''
+    Function checks to make sure the user gave the script the right number of commmand
+    line arguments.
+    
+    @rtype: tuple
+    @return: a tuple with the first value being the first command line argument and the second 
+                value being the second command line argument
+    ''' 
+    #check the user gave the right number of arguments
+    #if not print what the user should have given and then exit  
+    if len(sys.argv) < 4:
+        print("Usage: werthman-assgn1.py <absolute path to file of word list> <absolute path to file of hashtags> <word reference for WER>")
+        sys.exit()
+    else:
+        #check if the user gave the absolute path for both files otherwise print script usage and exit
+        if os.path.isabs(sys.argv[1]) == False or os.path.isabs(sys.argv[2]) == False or os.path.isabs(sys.argv[2]) == False:
+            print("Usage: werthman-assgn1.py <absolute path to file of word list> <absolute path to file of hashtags> <word reference for WER>")
+            sys.exit()
+        else:
+            #if the user gave the right number of arguments and both absolute paths return them
+            return (sys.argv[1], sys.argv[2], sys.argv[3])     
+
+def  minEditDist(target, source):
+    ''' Computes the min edit distance from target to source. Figure 3.25 in the book. Assume that
+    insertions, deletions and (actual) substitutions all cost 1 for this HW. Note the indexes are a
+    little different from the text. There we are assuming the source and target indexing starts a 1.
+    Here we are using 0-based indexing.
+    
+    @type string:
+    @param target: the string that the source should look like
+    @type string:
+    @param source: the string that the min edit algorithm will be performed on to 
+                    see how closely it resembles the target string
+                    
+    @rtype: float
+    @return: the number of insertions, deletions, and substitutions to make
+                the source look like the target
+    '''
+    
+    #number of rows of the matrix/table
+    n = len(target)
+    #number of columns of the matrix/table
+    m = len(source)
+
+    #create the n by m matrix 
+    distance = [[0 for i in range(m+1)] for j in range(n+1)]
+    
+    #create the base cases of the matrix 
+    #which ends up being the length of each substring
+    #1 2 3 4... down the rows
+    for i in range(1,n+1):
+        distance[i][0] = distance[i-1][0] + 1
+    
+    #1 2 3 4... across the columns
+    for j in range(1,m+1):
+        distance[0][j] = distance[0][j-1] + 1
+    
+    #fill in the rest of the matrix with costs
+    #insertions = 1, delete = 1, substitution = 0 if the words are equal, 2 if they are not
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            distance[i][j] = min(distance[i-1][j] + 1,
+                                 distance[i][j-1] + 1,
+                                 distance[i-1][j-1]+substCost(source[j-1],target[i-1]))
+    return float(distance[n][m])
+
+def substCost(source, target):
+    if source == target:
+        return 0
+    else:
+        return 1
+
+def runMaxMatchMinEdit(maxmatchFunction, wordlist, hashtaglist, referenceOutputPath):
+    #use the maxmatch algo and change the hashtags and add them to a list
+    print("Calculating the maxmatch of the hashtags.")
+    maxmatchHashtags = []
+    for hashtag in hashtaglist:
+        maxmatchHashtags.append(maxmatchFunction(hashtag, wordlist, ""))
+    
+    #write the maxmatched hashtags to a file
+    print("Writing the maxmatched hashtags to the file werthman-out-assgn1.txt")
+    with open('werthman_out_assgn1.txt', 'w') as f:
+        for maxmatchHashtag in maxmatchHashtags:
+            f.write("{0}\n".format(maxmatchHashtag))
+    
+    #caculcate the minimum edit distance between the hashtags I created and what
+    #they really should be
+    #load created hashtags into a set
+    maxmatchHashtags = []
+    with open('werthman_out_assgn1.txt', 'r') as f:
+        for line in f:
+            maxmatchHashtags.append(line.strip())
+            
+    #load correct hashtags into a set
+    correctHashtags = []
+    with open(referenceOutputPath, 'r') as f:
+        for line in f:
+            correctHashtags.append(line.strip())
+    
+    #go through each list and compare the strings
+    print("Calculating the average WER across the test set.")
+    totalWER = 0.0
+    for maxmatchHashtag, correctHashtag in zip(maxmatchHashtags, correctHashtags):
+        #convert each string to a list of the words in the string
+        maxmatchHashtagAsList = maxmatchHashtag.split()
+        correctHashtagAsList = correctHashtag.split()
+        #call min edit and add up wer for each string and then divide that by total number of lines in the hashtag file
+        totalWER += minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList)
+        
+    #to get the average word error rate across the entire test set
+    print("Average WER across test set is {0}".format(totalWER/len(correctHashtags)))
+
+def main():
+
+    #check that the user gave the right number of arguments and absolute paths
+    (wordlistPath, hashtaglistPath, referenceOutputPath) = checkCommandLineArgs()
+    print("Path to the word list: " + wordlistPath)
+    print("Path to the hashtag list: " + hashtaglistPath)
+    print("Path to the reference output: " + referenceOutputPath)
+    
+    wordlist = readWordsFromFile(wordlistPath, True, 75000)
+    hashtaglist = readWordsFromFile(hashtaglistPath, False, 0)
+    
+    print("----Running normal maxmatch function.----")
+    runMaxMatchMinEdit(maxMatch, wordlist, hashtaglist, referenceOutputPath)
+    print("----Running backwards maxmatch function----")
+    runMaxMatchMinEdit(backwardsMaxMatch, wordlist, hashtaglist, referenceOutputPath)
+    print("----Running any maxmatch function----")
+    runMaxMatchMinEdit(anyMaxMatch, wordlist, hashtaglist, referenceOutputPath)
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ DeterministicSegmentation/werthman_assgn1_tester.py
@@ -0,0 +1,142 @@
+'''
+Created on Sep 11, 2015
+
+@author: Bob
+'''
+import werthman_assgn1
+
+def assertion(boolean, testName):
+    if boolean:
+        print(testName + " PASSED")
+    else:
+        print(testName + " FAILED")
+        
+def createWordAndHashtagFiles():
+    wordlist = ['moon 5678', 'man 1234', 'over 345', 'the 5678', 'help 777777', 'me 9999',
+                 't 7777', 'fi 9999']
+    hashtaglist = ['#manoverthemoon', '#helpmeoverthere', '#findmefood', '#whaaaaat']
+    
+    #create the file with the test words
+    with open('testwordlist.txt', 'w') as f:
+        for word in wordlist:
+            f.write('{0}\n'.format(word))
+    
+    #create the file with the test hashtags     
+    with open('testhashtaglist.txt', 'w') as f:
+        for hashtag in hashtaglist:
+            f.write('{0}\n'.format(hashtag))
+              
+def testReadWordsFromFile():
+    testwordlist = ['moon', 'man', 'over', 'the', 'help', 'me',
+                 't', 'fi']
+    testhashtaglist = ['manoverthemoon', 'helpmeoverthere', 'findmefood', 'whaaaaat']
+    createWordAndHashtagFiles()
+    hashtaglist = werthman_assgn1.readWordsFromFile('testhashtaglist.txt', False, 20)
+    wordlist =  werthman_assgn1.readWordsFromFile('testwordlist.txt', True, 20)
+    
+    #tests that the right words and hashtags are in the lists
+    for word in wordlist:  
+        assertion(word in testwordlist, "Set contains {0}.".format(word))
+        
+    for hashtag in hashtaglist:  
+        assertion(hashtag in testhashtaglist, "Set contains {0}.".format(hashtag))
+    
+    #test that the word list is limited by a certain size
+    wordlist = werthman_assgn1.readWordsFromFile('testwordlist.txt', True, 2)
+    assertion(len(wordlist) == 2, "Word list should be size 2.")
+    
+def initialTestMaxMatchAlgo():
+    testwordlist = ['moon', 'man', 'over', 'the', 'help', 'me',
+                 't', 'fi']
+    testhashtaglist = ['manoverthemoon', 'helpmeoverthere', 'findmefood', 'whaaaaat']
+    for hashtag in testhashtaglist:
+        maxmatchedHashtag = ""
+        if hashtag == "manoverthemoon":
+            #test for the hashtag being made up of entirely of words in the word list
+            maxmatchedHashtag = werthman_assgn1.maxMatch("manoverthemoon", testwordlist, maxmatchedHashtag)
+            assertion(maxmatchedHashtag == "man over the moon", "manoverthemoon should be changed to man over the moon.")
+        elif hashtag == "helpmeoverthere":
+            #test for the hashtag being made up of some words from the word list
+            maxmatchedHashtag = werthman_assgn1.maxMatch("helpmeoverthere", testwordlist, maxmatchedHashtag)
+            assertion(maxmatchedHashtag == "help me over the r e", "helpmeoverthere should be changed to help me over the r e.")
+        elif hashtag == "whaaaaat":
+            #test for the hashtag being made up of none of the words in the wordlist
+            maxmatchedHashtag = werthman_assgn1.maxMatch("whaaaaat", testwordlist, maxmatchedHashtag)
+            assertion(maxmatchedHashtag == "w h a a a a a t", "whaaaaat should be changed to w h a a a a a t.")
+        elif hashtag == "findmefood":
+            #test for the hashtag being made up of none of the words in the wordlist
+            maxmatchedHashtag = werthman_assgn1.maxMatch("findmefood", testwordlist, maxmatchedHashtag)
+            assertion(maxmatchedHashtag == "fi n d me f o o d", "findmefood should be changed to fi n d me f o o d.")
+            
+def finalTestMaxMatchAlgo():
+    #retrieve the wordlist and hashtag list form the file system
+    wordlist = werthman_assgn1.readWordsFromFile('bigwordlist.txt', True, 75000)
+    hashtags = werthman_assgn1.readWordsFromFile('hashtags-train.txt', False, 0)
+    
+    #use the maxmatch algo and change the hashtags and add them to a list
+    maxmatchHashtags = []
+    for hashtag in hashtags:
+        maxmatchHashtags.append(werthman_assgn1.maxMatch(hashtag, wordlist, ""))
+        
+    #get the hashtags of the expected output of the maxmatch algo from professor's provided file
+    expectedHashtags = []
+    with open('hashtags-train-maxmatch.txt', 'r') as f:
+        for line in f:
+            #strip off whitespace characters like newlines
+            expectedHashtags.append(line.strip())
+    
+    #compare the hashtags created by the maxmatch algo to those provided by the professor
+    for maxmatchHashtag, expectedmaxmatchHashtag in zip(maxmatchHashtags, expectedHashtags):
+        assertion(maxmatchHashtag == expectedmaxmatchHashtag, "My maxmatch algo hashtag: {0} should be the same as the professor's maxmatch algo: {1}.".format(maxmatchHashtag, expectedmaxmatchHashtag))
+            
+def testMinEditDistanceAlgo():
+    #retrieve the wordlist and hashtag list form the file system
+    wordlist = werthman_assgn1.readWordsFromFile("testwordlist.txt", True, 75000)
+    hashtags = werthman_assgn1.readWordsFromFile("testhashtaglist.txt", False, 0)
+    
+    #use the maxmatch algo and change the hashtags and add them to a list
+    maxmatchHashtags = []
+    for hashtag in hashtags:
+        maxmatchHashtags.append(werthman_assgn1.maxMatch(hashtag, wordlist, ""))
+    
+    #read in the list of what the hashtags should really look like
+    correctHashtags = []
+    with open("realtesthashtags.txt", "r") as f:
+        for line in f:
+            correctHashtags.append(line.strip())
+    
+    #compare each maxmatchHashtag to each correctHashtag word by word
+    totalWER = 0.0
+    for maxmatchHashtag, correctHashtag in zip(maxmatchHashtags, correctHashtags):
+        #convert each string to a list of the words in the string
+        maxmatchHashtagAsList = maxmatchHashtag.split()
+        correctHashtagAsList = correctHashtag.split()
+        
+        #test WER for the the hashtags created by maxmatch
+        if correctHashtag == "man over the moon" :
+            #maxmatchHashtag is the same as the correctHashtag so no changes need to be made
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList) == 0, "Man over the moon should have a min edit distance of 0")
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList) == 0, "Man over the moon should have a WER of 0")
+            totalWER += werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList)
+        elif correctHashtag == "find me food":
+            #maxmatchHashtag:fi ndmefood requires two substitutions fi -> find and ndmefood -> me and an insertion of food
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList) == 7, "Find me food should have a min edit distance of 7")
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList) == 7.0/3, "Find me food should have a should have a WER of 7/3")
+            totalWER += werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList)
+        elif correctHashtag == "help me over there":
+            #masmatchHashtag:help me over the re requires a substitution the->there and a deletion of re
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList) == 3, "help me over there should have a min edit distance of 3")
+            assertion(werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList) == 3.0/4, "help me over there should have a WER of 3/4")
+            totalWER += werthman_assgn1.minEditDist(correctHashtagAsList, maxmatchHashtagAsList)/len(correctHashtagAsList)
+
+    #average the WER across of the hashtags
+    assertion(totalWER/len(correctHashtags) == ((7.0/3 + 3.0/4)/3.0), "Average WER across test set should be .58")
+    
+def main():
+    testReadWordsFromFile()
+    initialTestMaxMatchAlgo()
+    finalTestMaxMatchAlgo()
+    testMinEditDistanceAlgo()
+    
+if __name__ == '__main__':
+    main()
